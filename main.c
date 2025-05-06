03aa74d09fd71b44c4ae082fa0b18100658fdf90
Guillaume <xgold3nfir3@gmail.com>
2025-05-06 13:50:20 +1000

    minimalist spi test

--- main.c
+++ main.c
@@ -1,58 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <fcntl.h>
+#include <stdint.h>
 #include <unistd.h>
-#include <linux/spi/spidev.h>
 #include <sys/ioctl.h>
+#include <linux/spi/spidev.h>
 #include <string.h>
 
 #define DEVICE "/dev/spidev0.0"
-#define SPEED_HZ 1000000
-#define WORDS 8  // nb of 16-bit words to read each loop (BUFFER_SIZE on pico)
 
 int main() {
-    int spi_fd = open(DEVICE, O_RDWR);
-    if (spi_fd < 0) {
+    int fd = open(DEVICE, O_RDWR);
+    if (fd < 0) {
         perror("Failed to open SPI device");
         return 1;
     }
 
     uint8_t mode = SPI_MODE_0;
-    uint8_t bits_per_word = 8;
+    uint8_t bits = 8;
+    uint32_t speed = 1000000; // 1 MHz
 
-    ioctl(spi_fd, SPI_IOC_WR_MODE, &mode);
-    ioctl(spi_fd, SPI_IOC_WR_BITS_PER_WORD, &bits_per_word);
-    ioctl(spi_fd, SPI_IOC_WR_MAX_SPEED_HZ, &(uint32_t){SPEED_HZ});
+    ioctl(fd, SPI_IOC_WR_MODE, &mode);
+    ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
+    ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
 
-    uint8_t tx[WORDS * 2] = {0};
-    uint8_t rx[WORDS * 2];
+    uint8_t tx = 0x42;
+    uint8_t rx = 0;
 
-    while (1) {
-        struct spi_ioc_transfer transfer = {
-            .tx_buf = (unsigned long)tx,
-            .rx_buf = (unsigned long)rx,
-            .len = sizeof(tx),
-            .delay_usecs = 0,
-            .speed_hz = SPEED_HZ,
-            .bits_per_word = bits_per_word,
-        };
+    struct spi_ioc_transfer tr = {
+        .tx_buf = (unsigned long)&tx,
+        .rx_buf = (unsigned long)&rx,
+        .len = 1,
+        .speed_hz = speed,
+        .bits_per_word = bits,
+    };
 
-        int ret = ioctl(spi_fd, SPI_IOC_MESSAGE(1), &transfer);
-        if (ret < 0) {
-            perror("SPI transfer failed");
-            break;
-        }
-
-        printf("Received: ");
-        for (int i = 0; i < WORDS * 2; i++) {
-            printf("0x%02X ", rx[i]);
-        }
-        printf("\n");
-
-        sleep(1);
+    if (ioctl(fd, SPI_IOC_MESSAGE(1), &tr) < 1) {
+        perror("SPI transfer failed");
+        return 1;
     }
 
-    close(spi_fd);
+    printf("Pi received: 0x%02X\n", rx);
+    close(fd);
     return 0;
 }
