7c4f69d16b0565878a575c3680120a24c5533c72
Guillaume <xgold3nfir3@gmail.com>
2025-05-06 16:04:00 +1000

    interacting with pio bit-banged pico spi

--- main.c
+++ main.c
@@ -1,48 +1,52 @@
 #include <fcntl.h>
 #include <linux/spi/spidev.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 
-#define SPI_PATH "/dev/spidev0.0"
-#define BUF_LEN 16
+#define SPI_DEV "/dev/spidev0.0"
 
 int main() {
-    int fd = open(SPI_PATH, O_RDWR);
+    int fd = open(SPI_DEV, O_RDWR);
     if (fd < 0) {
         perror("open");
         return 1;
     }
 
-    uint8_t tx[BUF_LEN], rx[BUF_LEN];
-    for (int i = 0; i < BUF_LEN; ++i) tx[i] = i;
-
     uint8_t mode = SPI_MODE_0;
     ioctl(fd, SPI_IOC_WR_MODE, &mode);
+
     uint8_t bits = 8;
     ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
-    uint32_t speed = 1000000;
+
+    uint32_t speed = 1 * 1000 * 1000;  // 1 MHz
     ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
 
-    struct spi_ioc_transfer tr = {
-        .tx_buf = (unsigned long)tx,
-        .rx_buf = (unsigned long)rx,
-        .len = BUF_LEN,
-        .speed_hz = speed,
-        .bits_per_word = bits,
-    };
+    printf("Talking to Pico...\n");
 
-    if (ioctl(fd, SPI_IOC_MESSAGE(1), &tr) < 1) {
-        perror("SPI_IOC_MESSAGE");
-        return 1;
+    for (int i = 0; i < 10; ++i) {
+        uint8_t tx = 0x00;  // dummy byte
+        uint8_t rx = 0x00;
+
+        struct spi_ioc_transfer tr = {
+            .tx_buf = (unsigned long)&tx,
+            .rx_buf = (unsigned long)&rx,
+            .len = 1,
+            .speed_hz = speed,
+            .bits_per_word = bits,
+        };
+
+        if (ioctl(fd, SPI_IOC_MESSAGE(1), &tr) < 1) {
+            perror("SPI_IOC_MESSAGE");
+            close(fd);
+            return 1;
+        }
+
+        printf("Received from Pico: %d\n", rx);
+        sleep(1);
     }
-
-    printf("Received: ");
-    for (int i = 0; i < BUF_LEN; i++) printf("%02X ", rx[i]);
-    printf("\n");
 
     close(fd);
     return 0;
