0765cdc4168e9a08f7439c094a0134a166901588
Guillaume <xgold3nfir3@gmail.com>
2025-05-06 14:25:37 +1000

    using another example

--- main.c
+++ main.c
@@ -1,48 +1,47 @@
-#include <stdio.h>
 #include <fcntl.h>
-#include <unistd.h>
-#include <stdint.h>
-#include <sys/ioctl.h>
 #include <linux/spi/spidev.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
-#define DEVICE "/dev/spidev0.0"
+#define SPI_PATH "/dev/spidev0.0"
+#define BUF_LEN 16
 
 int main() {
-    int fd = open(DEVICE, O_RDWR);
+    int fd = open(SPI_PATH, O_RDWR);
     if (fd < 0) {
-        perror("Failed to open SPI device");
+        perror("open");
         return 1;
     }
 
+    uint8_t tx[BUF_LEN], rx[BUF_LEN];
+    for (int i = 0; i < BUF_LEN; ++i) tx[i] = i;
+
     uint8_t mode = SPI_MODE_0;
-    uint8_t bits = 8;
-    uint32_t speed = 1000000;
-
     ioctl(fd, SPI_IOC_WR_MODE, &mode);
+    uint8_t bits = 8;
     ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
+    uint32_t speed = 1000000;
     ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
-
-    uint8_t tx[16], rx[16];
-    for (int i = 0; i < 16; i++) tx[i] = i;
 
     struct spi_ioc_transfer tr = {
         .tx_buf = (unsigned long)tx,
         .rx_buf = (unsigned long)rx,
-        .len = 16,
-        .delay_usecs = 0,
+        .len = BUF_LEN,
         .speed_hz = speed,
         .bits_per_word = bits,
     };
 
-    if (ioctl(fd, SPI_IOC_MESSAGE(1), &tr) < 0) {
-        perror("SPI transfer failed");
+    if (ioctl(fd, SPI_IOC_MESSAGE(1), &tr) < 1) {
+        perror("SPI_IOC_MESSAGE");
         return 1;
     }
 
-    printf("Received from Pico: ");
-    for (int i = 0; i < 16; i++) {
-        printf("%02X ", rx[i]);
-    }
+    printf("Received: ");
+    for (int i = 0; i < BUF_LEN; i++) printf("%02X ", rx[i]);
     printf("\n");
 
     close(fd);
